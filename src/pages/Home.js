b2d65127399055b4c97adc0ed4f0a503bdf03aca
shalawfatah <shalaw.fatah@gmail.com>
2021-04-28 00:12:14 -0700

    added posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
 import '../App.css'
+import Posts from '../components/Posts'
 
 const Home = () => {
     const POST_URL = 'https://jsonplaceholder.typicode.com/posts'
@@ -19,6 +20,13 @@ const Home = () => {
             <p>
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
             </p>
+            <div>
+                {posts.map((post)=> {
+                    return (
+                        <Posts title={post.title} body={post.body} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
