e1300fd332e1c64fc67e70339b4f5f2238c3ae92
shalawfatah <shalaw.fatah@gmail.com>
2021-04-27 23:55:50 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import News from './pages/News';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/about' exact>
+        <About />
+      </Route>
+      <Route path='/news' exact>
+        <News />
+      </Route>
+      <Route path='/contact' exact>
+        <Contact />
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
