7c6a90f2d558b9a25ff4f1dec73083c46e5267a9
shalawfatah <shalaw.fatah@gmail.com>
2021-04-28 00:15:36 -0700

    added load more button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Posts from '../components/Posts'
 const Home = () => {
     const POST_URL = 'https://jsonplaceholder.typicode.com/posts'
     const [posts, setPosts] = useState([])
+    const [visible, setVisible] = useState(5)
     const fetchPosts = async () => {
         const response = await fetch(POST_URL)
         const data = await response.json()
@@ -21,12 +22,13 @@ const Home = () => {
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
             </p>
             <div>
-                {posts.map((post)=> {
+                {posts.slice(0, visible).map((post)=> {
                     return (
                         <Posts title={post.title} body={post.body} />
                     )
                 })}
             </div>
+            <button>See More</button>
         </div>
     )
 }
