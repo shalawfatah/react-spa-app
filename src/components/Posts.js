d1f4ca1ed7046ccfa48a47fcd680eb9f3822b6e1
shalawfatah <shalaw.fatah@gmail.com>
2021-04-28 00:14:06 -0700

    added styles

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Posts = ({title, body}) => {
     return (
         <div>
-            <h1>{title}</h1>
+            <h4 style={{backgroundColor: 'silver', borderRadius: '12px', padding:'12px'}}>{title}</h4>
             <p>{body}</p>
         </div>
     )
