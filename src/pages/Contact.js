bed45bca7a34a59f431c284ec472e660c53e326f
shalawfatah <shalaw.fatah@gmail.com>
2021-04-28 00:38:16 -0700

    added font weight

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -25,7 +25,7 @@ const Contact = () => {
                         <h3>{us.name}</h3>
                         <p style={{color:'gray'}}>{us.email}</p>
                         <p>{us.phone}</p>
-                        <p style={{color:'blue'}}>{us.website}</p>
+                        <p style={{color:'blue', fontWeight="700"}}>{us.website}</p>
                         <p style={{color: 'white', backgroundColor:'gray', padding:'4px'}}>{us.company.name}</p>
                         <hr />
                         </>
